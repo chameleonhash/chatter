aa279407b08cd89d56e31b78b327c9e209763239
chameleonhash <chameleonhash@gmail.com>
2024-02-24 15:23:04 -0500

    Update llama2chatter.py

--- llama2chatter.py
+++ llama2chatter.py
@@ -25,9 +25,9 @@ with st.sidebar:
         llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
     elif selected_model == 'Llama2-13B':
         llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
-    Creativity = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-    Focus = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    Length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+    temperature = st.sidebar.slider('Creativity', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('Focus', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('Response_Length', min_value=32, max_value=128, value=120, step=8)
     st.markdown('Follow me on [TikTok](https://tiktok.com/@chameleonhash/)!')
 
 # Store LLM generated responses
